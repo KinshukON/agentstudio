dfde0bd3d102dc80bcceffca8e0adfd200dc7284
kinshukdutta <kinshuk.dutta@tanium.com>
2025-12-24 18:39:02 -0500

    Fix: Disable missing suspense error for useSearchParams

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable static page generation for pages that use useSearchParams
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
